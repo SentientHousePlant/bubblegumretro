ff3d0d8801b66d676335844152120d9909ea8b91
Jack <jackjharrington@icloud.com>
2021-09-19 14:40:35 +0100

    Updated Entry Point.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Box from "./components/Box/Box";
 import GlobalStyle from "./components/BubblegumGlobalStyle/BubblegumGlobalStyle";
+import Spacer from "./components/utils/spacer/spacer";
+import BubbleButton from "./components/Buttons/BaseButton";
 
-export { GlobalStyle, Box };
+export { GlobalStyle, Box, BubbleButton, Spacer };
